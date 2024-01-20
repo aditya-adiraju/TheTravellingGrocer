459668dad00ab599fd1d998f51cc97afb3b6636b
cwr <colwynralph15@gmail.com>
2024-01-20 14:27:15 -0800

    Added http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, importProvidersFrom} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SampleService} from "./services/sample.service";
-import {HttpClientModule} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TheTravellingGrocer';
-  constructor(public sample: SampleService) {
-    // sample.sampleAPICall().pipe().subscribe(()=>console.log("YAY"))
+  constructor(private sample:SampleService) {
+    sample.sampleAPICall().subscribe((value:any)=>console.log(value))
   }
 }
