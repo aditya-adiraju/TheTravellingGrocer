7d76b5e61babf2998c6b3f44a6680291d1f66ca6
parm-is-coding <pardhi1400@gmail.com>
2024-01-21 00:53:29 -0800

    add and route go button to location page

---
+++ client/app/pages/location/location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './location.component.html',
+  styleUrl: './location.component.css'
+})
+export class LocationComponent {
+
+}
--- client/app/pages/signin-page/signin-page.component.ts
+++ client/app/pages/signin-page/signin-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { TextFieldComponent } from 'client/app/shared/text-field/text-field.component';
+import { OutlineButtonComponent } from 'client/app/shared/outline-button/outline-button.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin-page',
   standalone: true,
-  imports: [],
+  imports: [TextFieldComponent,OutlineButtonComponent],
   templateUrl: './signin-page.component.html',
   styleUrl: './signin-page.component.css'
 })
 export class SigninPageComponent {
-
+  constructor(private router:Router){}
+  buttonText = "Go";
+  username = "UserName";
+  password = "Password";
+  goToLocation(){
+    console.log("Hello");
+    this.router.navigate(["location"]);
+  }
 }
