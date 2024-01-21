9d2779999f7aa534b5f58363d245a137c9d05070
parm-is-coding <pardhi1400@gmail.com>
2024-01-21 01:31:31 -0800

    added shopping page

--- client/app/pages/location/location.component.ts
+++ client/app/pages/location/location.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { TextFieldComponent } from 'client/app/shared/text-field/text-field.component';
+import { OutlineButtonComponent } from 'client/app/shared/outline-button/outline-button.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [],
+  imports: [TextFieldComponent,OutlineButtonComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
+  constructor(private router:Router){}
+  done = "Done";
+  goToShopping(){
+    this.router.navigate(["shopping"]);
 
+  }
 }
---
+++ client/app/pages/shopping/shopping.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping.component.html',
+  styleUrl: './shopping.component.css'
+})
+export class ShoppingComponent {
+
+}
