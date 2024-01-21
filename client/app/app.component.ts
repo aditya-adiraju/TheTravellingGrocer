966a71376939f6f50857b5457726d023925e0df2
cwr <colwynralph15@gmail.com>
2024-01-21 11:39:05 -0800

    tsp in typescript

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -4,6 +4,7 @@ import { SampleService } from "./services/sample.service";
 import { ShoppingListComponent } from "./shared/shopping-list/shopping-list.component";
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
+import {solveTSP} from "./utils/tsp";
 
 @Component({
   selector: 'app-root',
@@ -18,5 +19,14 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   title = 'TheTravellingGrocer';
   constructor() {
+    const adjacencyMatrix: number[][] = [
+      [0, 1, 2, 0],
+      [1, 0, 0, 3],
+      [2, 0, 0, 4],
+      [0, 3, 4, 0]
+    ];
+
+    const result: number[] = solveTSP(adjacencyMatrix);
+    console.log(result);
   }
 }
---
+++ client/app/utils/tsp.ts
@@ -0,0 +1,60 @@
+class CompareFirstElement {
+  public compare(left: [number, [number, number]], right: [number, [number, number]]): number {
+    return left[0] - right[0];
+  }
+}
+
+class Node {
+  num: number = 0;
+  neighbours: number[] = [];
+
+  constructor() {
+    this.neighbours = [];
+  }
+}
+
+export function solveTSP(adj: number[][]): number[] {
+  const visited: boolean[] = new Array(adj.length).fill(false);
+  const pq: [number, [number, number]][] = [];
+  const compareFirstElement = new CompareFirstElement();
+
+  pq.push([0, [0, 0]]);
+
+  const nodes: Node[] = new Array(adj.length).fill(null).map(() => new Node());
+
+  while (pq.length > 0) {
+    const [cost, [parent, current]] = pq.shift()!;
+    if (visited[current]) continue;
+
+    nodes[parent].neighbours.push(current);
+    nodes[current].neighbours.push(parent);
+
+    visited[current] = true;
+    for (let i = 0; i < adj.length; i++) {
+      if (adj[current][i] !== 0 && !visited[i]) {
+        pq.push([adj[current][i], [current, i]]);
+      }
+    }
+    pq.sort(compareFirstElement.compare);
+  }
+
+  const order: number[] = [];
+  const q: number[] = [];
+  q.push(0);
+  visited.fill(false);
+
+  while (q.length > 0) {
+    const u = q.shift()!;
+    if (visited[u]) continue;
+    visited[u] = true;
+    order.push(u);
+
+    for (const v of nodes[u].neighbours) {
+      if (!visited[v]) {
+        q.push(v);
+      }
+    }
+  }
+
+  return order;
+}
