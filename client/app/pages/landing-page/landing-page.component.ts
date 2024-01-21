a56a2a622dc489060822a2795ee73d82f534a4cb
Eric <eyee01@students.cs.ubc.ca>
2024-01-21 09:52:29 -0800

    made dashboard

--- client/app/pages/landing-page/landing-page.component.ts
+++ client/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OutlineButtonComponent } from '../../shared/outline-button/outline-button.component';
+import { LoginButtonComponent } from 'client/app/shared/login-button/login-button.component';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [OutlineButtonComponent,],
+  imports: [OutlineButtonComponent, LoginButtonComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
