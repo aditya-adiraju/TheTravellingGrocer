5245d7426315bfedd77997432cc1053006a45a37
cwr <colwynralph15@gmail.com>
2024-01-21 11:31:56 -0800

    Fixed search bar

--- client/main.ts
+++ client/main.ts
@@ -52,12 +52,9 @@ export class DataService {
           .set('Authorization', 'my-auth-token')
           .set('Content-Type', 'application/json');
 
-    this.http.post('http://localhost:3000/api/data/getAllFilteredItems', JSON.stringify({query: q}), {
+    return this.http.post('http://localhost:3000/api/data/getAllFilteredItems', JSON.stringify({query: q}), {
       headers: headers
     })
-    .subscribe(data => {
-      console.log(data);
-    });
   }
 }
 
