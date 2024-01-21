28c53f763f6179248505c11f252014a3c2b273f1
Eric <eyee01@students.cs.ubc.ca>
2024-01-21 11:58:44 -0800

    final commit

--- client/app/pages/map/map.component.ts
+++ client/app/pages/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MappedinLocation, MappedinDestinationSet, getVenue, showVenue, MappedinPolygon, E_SDK_EVENT } from "@mappedin/mappedin-js";
+import { MappedinLocation, MappedinDestinationSet, getVenue, showVenue, MappedinPolygon, E_SDK_EVENT, Mappedin } from "@mappedin/mappedin-js";
 import { augmentedPolygonThings } from "./defaultThings";
 import productData from "./products.json";
 import {DataService} from "../../../main";
@@ -8,12 +8,14 @@ import { ShoppingListManagerService } from 'client/app/services/shopping-list-ma
 import {takeUntil} from "rxjs";
 import {GarbageCollectorComponent} from "../../shared/garbage-collector/garbage-collector.component";
 
+
 const options = {
   venue: "mappedin-demo-retail-2",
   clientId: "5eab30aa91b055001a68e996",
   clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
   things: augmentedPolygonThings // ensures polygon name is fetched
 };
+
 
 @Component({
   selector: 'app-map',
@@ -32,31 +34,15 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
     this.dataService.getAllItems().subscribe((data)=>console.log(data))
   }
 
-  async ngOnInit(): Promise<void> {
-    const venue = await getVenue(options);
-    const mapView = await showVenue(this.mapEl.nativeElement, venue);
-
-    let products = new Array();
-    this.shopService.getAccessibleShoppingList().pipe(
-      takeUntil(this.unsubscribe)
-    ).subscribe((list)=>{
-      products = list;
-    });
-    // resultArray = ["Unico Pasta Sauce, Original", "Tortillas", "Cadbury Caramilk Dome Cake"]; TEST 
-
-
-    // //SAMPLE STRING
-    // let products: string[] = ["Unico Pasta Sauce, Original", "Tortillas", "Cadbury Caramilk Dome Cake"];
-
-    //Size of 2D array is length +1 so for the starting entrance
+  private calculateDistances(products: string[], venue: Mappedin) {
     const size = products.length + 1;
-    let twoDArray: number[][] = Array.from({length:size}, () => Array(size).fill(0));
+    let twoDArray: number[][] = Array.from({ length: size }, () => Array(size).fill(0));
 
     //Initialize first row/column based on entrance
     let i = 0;
     let start = venue.locations.find((l) => l.name == "Entrance")!;
     for (let j = i + 1; j < size; j++) {
-      let product2 = productData.find((w:any) => w.name === products[j-1]);
+      let product2 = productData.find((w: any) => w.name === products[j - 1]);
       let end = venue.polygons.find((p) => p.name === product2?.polygonName);
 
       let directionsDistance = start?.directionsTo(end!).distance!;
@@ -66,10 +52,10 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
 
     //Find rest of distances of products to other products
     for (let i = 1; i < size; i++) {
-      let product1 = productData.find((v:any) => v.name === products[i-1]);
+      let product1 = productData.find((v: any) => v.name === products[i - 1]);
       let start = venue.polygons.find((p) => p.name === product1?.polygonName);
       for (let j = i + 1; j < size; j++) {
-        let product2 = productData.find((w:any) => w.name === products[j-1]);
+        let product2 = productData.find((w: any) => w.name === products[j - 1]);
         let end = venue.polygons.find((p) => p.name === product2?.polygonName);
 
         let directionsDistance = start?.directionsTo(end!).distance!;
@@ -77,27 +63,92 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
         twoDArray[j][i] = directionsDistance;
       }
     }
+    return twoDArray;
+  }
 
-    console.log(twoDArray);
-    let inputArray = new Array();
+
+  async ngOnInit(): Promise<void> {
+    const venue = await getVenue(options);
+    const mapView = await showVenue(this.mapEl.nativeElement, venue);
+
+
+    let resultArray = new Array();
+    // this.shopService.getAccessibleShoppingList().pipe(
+    //   takeUntil(this.unsubscribe)
+    // ).subscribe((list)=>{
+    //   resultArray = list;
+    // });
+    resultArray = ["Unico Pasta Sauce, Original", "Tortillas", "Cadbury Caramilk Dome Cake"];
+
+
+
+
+    // //SAMPLE STRING
+    // let products: string[] = ["Unico Pasta Sauce, Original", "Tortillas", "Cadbury Caramilk Dome Cake"];
+
+
+    // //Size of 2D array is length +1 so for the starting entrance
+    // const size = products.length + 1;
+    // let twoDArray: number[][] = Array.from({length:size}, () => Array(size).fill(0));
+
+
+    // //Initialize first row/column based on entrance
+    // let i = 0;
+    // let start = venue.locations.find((l) => l.name == "Entrance")!;
+    // for (let j = i + 1; j < size; j++) {
+    //   let product2 = productData.find((w:any) => w.name === products[j-1]);
+    //   let end = venue.polygons.find((p) => p.name === product2?.polygonName);
+
+
+    //   let directionsDistance = start?.directionsTo(end!).distance!;
+    //   twoDArray[i][j] = directionsDistance;
+    //   twoDArray[j][i] = directionsDistance;
+    // }
+
+
+    // //Find rest of distances of products to other products
+    // for (let i = 1; i < size; i++) {
+    //   let product1 = productData.find((v:any) => v.name === products[i-1]);
+    //   let start = venue.polygons.find((p) => p.name === product1?.polygonName);
+    //   for (let j = i + 1; j < size; j++) {
+    //     let product2 = productData.find((w:any) => w.name === products[j-1]);
+    //     let end = venue.polygons.find((p) => p.name === product2?.polygonName);
+
+
+    //     let directionsDistance = start?.directionsTo(end!).distance!;
+    //     twoDArray[i][j] = directionsDistance;
+    //     twoDArray[j][i] = directionsDistance;
+    //   }
+    // }
+
+
+    // console.log(twoDArray);
+
 
     // //SEND THE MATRIX TO THE ALGORITHM AND WAIT FOR IT HERE
 
+
     // //SAMPLE DISTANCE LIST
     // let inputArray = new Array(74.99069792009288, 32.04512374311785, 25.18052353582995);
-    let resultArray = new Array();
+    // let resultArray = new Array();
 
-    //Turn the list of distance values into a list of items based on the 2D array
-    let startIndex = 0;
-    for (let i = 0; i < inputArray.length; i++) {
-      startIndex = twoDArray[startIndex].findIndex((num) => num == inputArray[i]);
-      resultArray.push(products[startIndex-1]);
-    }
 
-    console.log(resultArray);
+    // //Turn the list of distance values into a list of items based on the 2D array
+    // let startIndex = 0;
+    // for (let i = 0; i < inputArray.length; i++) {
+    //   startIndex = twoDArray[startIndex].findIndex((num) => num == inputArray[i]);
+    //   resultArray.push(products[startIndex-1]);
+    // }
+
+
+    // console.log(resultArray);
+
+    
+
 
 
     const startLocation = venue.locations.find((l) => l.name == "Entrance")!;
+
 
     //Create an array of locations to use as waypoints for a
     //multi-destination journey.
@@ -107,11 +158,15 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
       destinations.push(venue.polygons.find((p) => p.name === product?.polygonName)!);
     }
 
+
     console.log(destinations);
+
+
 
 
     //Get directions from the start location to all destinations.
     const directions = startLocation.directionsTo(new MappedinDestinationSet(destinations));
+
 
     //Pass the directions to Journey to be drawn on the map.
     //Set the paths as interactive so the user can click to
@@ -128,6 +183,7 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
       }
     });
 
+
     //Clickable functionality here
     let step = 0;
     mapView.on(E_SDK_EVENT.CLICK, (payload) => {
@@ -139,8 +195,11 @@ export class MapComponent extends GarbageCollectorComponent implements OnInit {
       }
     });
 
+
   }
+
 
   @ViewChild("app", { static: false }) mapEl!: ElementRef<HTMLElement>;
 }
+
 
